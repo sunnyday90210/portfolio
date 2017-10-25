b1623d7e556f5bbb540f693f2f6a36dac1ac7fa0
Harshit <pantharshit00@gmail.com>
2017-10-25 18:03:03 +0530

    All Set

---
+++ email.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+const direct = require("nodemailer-direct-transport");
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "279322d667f307",
+    pass: "195d5d728266ad"
+  }
+});
+
+module.exports = (name, email, message) => {
+  const mailOptions = {
+    from: `"${name}" <${email}>`, // sender address
+    to: "robertjohnwollny@gmail.com", // list of receivers
+    subject: "Contact request", // Subject line
+    text: message, // plain text body
+    html: `
+    <h1>Contact Request</h1>
+    <h3>From ${name}</h3>
+    <h3>Email ${email}</h3>
+
+    <p>${message}</p>
+    ` // html body
+  };
+
+  return transport.sendMail(mailOptions);
+};
